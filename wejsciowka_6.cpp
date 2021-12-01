0051ebe415ac0f5588448a8307bff6f7bd3b10ab
Marceli Sobiecki <64848895+MrMarox@users.noreply.github.com>
2021-12-02 00:05:21 +0100

    Update wejsciowka_6.cpp

--- wejsciowka_6.cpp
+++ wejsciowka_6.cpp
@@ -1,26 +1,25 @@
-﻿#include <vector>
+#include <vector>
 #include <iostream>
 using namespace std;
 
-void wypisz(int x)
+void wypisz(int x) 
 {
 	cout << x << endl;
 }
-
 void wypisz(vector<int>tab)
 {
-	for (int i = 0; i < tab.size(); i++)
+	for(int i = 0; i < tab.size();i++)
 	{
 		cout << tab[i] << " ";
+
 	}cout << endl;
 }
 
-int min(vector<int>tab)
-{
+int min(vector<int>tab) {
 	int wynik = tab[0];
 	for (int i = 0; i < tab.size(); i++)
 	{
-		if (tab[i] < wynik)
+		if (wynik < tab[i])
 		{
 			wynik = tab[i];
 		}
@@ -42,82 +41,77 @@ bool czyzawiera(vector<int>tab, vector<int>tab2)
 {
 	for (int i = 0; i < tab.size(); i++)
 	{
-		if (!czynalezy(tab, tab2[i]))
-		{
+		if (!czynalezy(tab, tab[i])) {
 			return false;
 		}
 	}return true;
 }
 
-vector<int> unikalnosc(vector<int>tab)/*<---- atrybut */
+vector<int> unikalnosc(vector<int>tab)
 {
 	vector<int>wynik;
-	for (int i = 0; i < tab.size(); i++) {
-		if (!czynalezy(wynik, tab[i])) {
+	for (int i = 0; i < tab.size(); i++)
+	{
+		if (!czynalezy(wynik, tab[i]))
+		{
 			wynik.push_back(tab[i]);
 		}
-	}	return wynik;
+	}return wynik;
 }
 
 vector <vector<int>> czestosc(vector<int>tab) {
 	vector<vector<int>>wynik;
 	vector<int>u = unikalnosc(tab);
 	vector<int>c;
-
 	for (int i = 0; i < u.size(); i++) {
 		int element = u[i];
 		int licznik = 0;
-		for (int j = 0; j < tab.size(); j++) {
-			if (element == tab[j]) {
+		for (int j = 0; j < tab.size(); j++)
+		{
+			if (element == tab[j]) 
+			{
 				licznik++;
 			}
-		}
-		c.push_back(licznik);
+		}c.push_back(licznik);
 	}
 	wynik.push_back(u);
 	wynik.push_back(c);
 	return wynik;
 }
-void wypisz(vector<vector<int>>tab) {
-	for (int j = 0; j < tab[1].size(); j++) {
+
+void wypisz(vector<vector<int>>tab)
+{
+	for (int j = 0; j < tab[1].size(); j++)
+	{
 		for (int i = 0; i < tab.size(); i++)
+		{
 			cout << tab[i][j] << " ";
-		cout << "\n";
+			
+		}cout << endl;
 	}
 }
-
 int main()
 {
-	int a, b, element;
-	a = 5;
-	b = 5;
-	element = 3;
-	vector<int>tab = { 1,2,3 };
-	vector<int>tab2 = { 3,2,1 };
-
+	int element = 2;
+	vector<int>tab = { 1,1,1,3,4,5 };
+	vector<int>tab2 = { 1,3,4,5 };
+	
 	wypisz(tab);
+	wypisz(unikalnosc(tab));
 	wypisz(min(tab));
 
-	if (czynalezy(tab, element) == true)
-	{
+	if (czynalezy(tab, element) == true) {
 		cout << "nalezy" << endl;
 	}
-	else
-	{
+	else {
 		cout << "nie nalezy" << endl;
 	}
 
-	if (czyzawiera(tab, tab2) == true)
-	{
+	if (czyzawiera(tab, tab2) == true) {
 		cout << "zawiera" << endl;
 	}
-	else
-	{
+	else {
 		cout << "nie zawiera" << endl;
 	}
-
-	wypisz(unikalnosc(tab));
-
-	cout << "Zliczone wartosci z tablicy:\n";
 	wypisz(czestosc(tab));
 }
